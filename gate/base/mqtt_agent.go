ea86901220d5648f1108b9c171d366159654bccf
liangdas <1587790525@qq.com>
2017-11-27 09:44:19 +0800

    =调整客户端连接的缓存

--- gate/base/mqtt_agent.go
+++ gate/base/mqtt_agent.go
@@ -60,8 +60,8 @@ func NewMqttAgent(module module.RPCModule)*agent{
 func (this *agent) OnInit(gate gate.Gate,conn network.Conn)error{
 	this.conn=conn
 	this.gate=gate
-	this.r=bufio.NewReader(conn)
-	this.w=bufio.NewWriter(conn)
+	this.r=bufio.NewReaderSize(conn,256)
+	this.w=bufio.NewWriterSize(conn,256)
 	this.isclose=false
 	this.rev_num=0
 	this.send_num=0
