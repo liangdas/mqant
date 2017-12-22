8e16bb5801b10de2290520cb1ca722993b7339ea
liangdas <1587790525@qq.com>
2017-12-22 09:53:45 +0800

    =修复随机数生成函数

--- utils/randInt.go
+++ utils/randInt.go
@@ -16,7 +16,7 @@ package utils
 import "math/rand"
 
 func RandInt64(min, max int64) int64 {
-	if min >= max || min == 0 || max == 0 {
+	if min >= max{
 		return max
 	}
 	return rand.Int63n(max-min) + min
