1471cfaacfd90f458597c139dc727b31e1cf76e1
jarekzha <jarekzha@gmail.com>
2018-05-09 21:16:06 +0800

    调整redis连接的默认配置

--- utils/redis.go
+++ utils/redis.go
@@ -41,7 +41,7 @@ func (this RedisFactory) GetPool(url string) *redis.Pool {
 
 	pool := &redis.Pool{
 		// 最大的空闲连接数，表示即使没有redis连接时依然可以保持N个空闲的连接，而不被清除，随时处于待命状态
-		MaxIdle: 10,
+		MaxIdle: 100,
 		// 最大的激活连接数，表示同时最多有N个连接 ，为0事表示没有限制
 		MaxActive: 100,
 		//最大的空闲连接等待时间，超过此时间后，空闲连接将被关闭
